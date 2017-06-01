96e67c9ad89979c52a8c38214c72e22902ad47e8
Kevin Wong <k.wong4@yahoo.com>
2017-06-01 13:43:08 -0400

    updated repo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,36 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def new
+		@user = User.new
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def create
+		render json: params
+	end
+
+	def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  	def user_params
+  		{
+  			utf
+  		}
+
 end
